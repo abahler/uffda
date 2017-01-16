b9576f867c32b1d7cd8a46a4d2d3aa651db2e51a
Alex Bahler <alexbahler@gmail.com>
2017-01-16 04:17:35 +0000

    Set up simple echo of message from server back to client

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let socket_io = require('socket.io');
 let http = require('http');
 let express = require('express');
@@ -9,7 +11,10 @@ let io = socket_io(server);
 
 io.on('connection', (socket) => {   // A Socket.IO server is an EventEmitter, so we listen for events
     console.log('Client connected');
-    console.log('Socket information: ', socket);
+
+    socket.on('message', (message) => {
+        console.log('Received message: ', message);
+    });
 });
 
 server.listen(process.env.PORT || 8080);    // server.listen replaces app.listen when we add in http and socket_io
