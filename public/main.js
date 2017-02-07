f2103177a0bd9d6ce5ab9b7329dceb5dd43b9179
Alex Bahler <alexbahler@gmail.com>
2017-02-07 04:16:10 +0000

    Allow user to set nickname; show names to each connection

--- public/main.js
+++ public/main.js
@@ -12,8 +12,17 @@ $(document).ready( () => {
     
     let showUserCount = (u) => {
         console.log('The showUserCount function is being called!');
-        console.log('u: ', u);          // Are we even getting anything?
+        console.log('u: ', u);          // Gets whatever the server passes to us
         userCount.html(u);
+    };
+    
+    let showUserList = (list) => {
+        console.log('`list` variable (list of nicknames): ', list);
+        let listHTML = '';
+        list.forEach( (v, i) => {
+            listHTML += '<li>' + v + '</li>';
+        });
+        userList.html(listHTML);
     };
 
     input.on('keydown', (event) => {
@@ -25,10 +34,18 @@ $(document).ready( () => {
         addMessage(message);
         socket.emit('message', message);    // First arg is a name for our message. We'll keep it simple.
         input.val('');
+        // Let users set a nickname Foobar by typing "My nickname is: Foobar"
+        if (message.indexOf('My nickname is: ') === 0) {
+            let result = message.match(/\s(\w+)$/);
+            let nickname = result[1];
+            socket.emit('newNickname', nickname);
+        }
     });
     
     socket.on('message', addMessage);   // When the server sends us a message with the name 'message', 
                                         // we add the attached data to the proper div via addMessage
     
     socket.on('updateNumUsers', showUserCount);
+    
+    socket.on('updateUserList', showUserList);
 });
